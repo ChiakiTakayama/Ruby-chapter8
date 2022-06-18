f2db37d4c91efc183ea8b2a2cabb7ed9893d2a25
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-18 01:55:49 +0000

    [Update]Ruby8章

--- greeting.rb
+++ greeting.rb
@@ -1,5 +1,7 @@
-def greeting
-  'Hello'
+def greeting(name)
+  return "Hello, #{name}!"#この行が戻り値。returnによって戻り値がこの行に
+  "Good morning, #{name}!"#この行を追加したことにより戻り値がこの行に変わる
+  
 end
 
-puts greeting
+puts greeting('John')
