43f7a09bc4250f217e1dd31ec3b56e453c5bc3f2
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-18 02:43:27 +0000

    [Update]何がわからないかわからない

---
+++ fizz_buzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz(number)
+  if
+    # 3の倍数とかでfizz
+  elsif
+    # 5の倍数とかでBuzz
+  elsif
+    # 15でFizzBuzz
+  elsif
+    # 他の数字の結果
+  end
+
+puts "数字を入力してください。"
+input = gets.to_i
+
+puts"結果は..."
